daa7323dbbff5517d7ce0b6ddab27115bca7a9ab
AmeenAhmed1 <ameenahmed386@gmail.com>
2020-05-29 16:44:42 +0200

    Update: Project2 - Score App

--- ScoreApp/app/src/main/java/com/ameen/scoreapp/MainActivity.java
+++ ScoreApp/app/src/main/java/com/ameen/scoreapp/MainActivity.java
@@ -12,11 +12,14 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView teamAScore, teamBScore;
-    Button teamAOne, teamATwo, teamAThree;
-    Button teamBOne, teamBTwo, teamBThree;
+    TextView teamAFoul, teamBFoul;
+    Button teamAOne, teamATwo, teamAThree, teamASix;
+    Button teamBOne, teamBTwo, teamBThree, teamBSix;
+    Button bTeamAFoul, bTeamBFoul;
     Button resetButton;
 
     int scoreA = 0, scoreB = 0;
+    int aFoul = 0, bFoul = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +30,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         teamAScore.setText(String.valueOf(scoreA));
         teamBScore = findViewById(R.id.textTeamBScore);
         teamBScore.setText(String.valueOf(scoreB));
+        teamAFoul = findViewById(R.id.textTeamAFoul);
+        teamAFoul.setText(String.valueOf(aFoul));
+        teamBFoul = findViewById(R.id.textTeamBFoul);
+        teamBFoul.setText(String.valueOf(bFoul));
 
         teamAOne = findViewById(R.id.btnTeamAOne);
         teamAOne.setOnClickListener(this);
@@ -34,6 +41,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         teamATwo.setOnClickListener(this);
         teamAThree = findViewById(R.id.btnTeamAThree);
         teamAThree.setOnClickListener(this);
+        teamASix = findViewById(R.id.btnTeamASix);
+        teamASix.setOnClickListener(this);
+        bTeamAFoul = findViewById(R.id.btnTeamAFoul);
+        bTeamAFoul.setOnClickListener(this);
 
         teamBOne = findViewById(R.id.btnTeamBOne);
         teamBOne.setOnClickListener(this);
@@ -41,6 +52,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         teamBTwo.setOnClickListener(this);
         teamBThree = findViewById(R.id.btnTeamBThree);
         teamBThree.setOnClickListener(this);
+        teamBSix = findViewById(R.id.btnTeamBSix);
+        teamBSix.setOnClickListener(this);
+        bTeamBFoul = findViewById(R.id.btnTeamBFoul);
+        bTeamBFoul.setOnClickListener(this);
 
         resetButton = findViewById(R.id.btnReset);
         resetButton.setOnClickListener(this);
@@ -51,6 +66,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onSaveInstanceState(@NonNull Bundle outState) {
         outState.putInt("ScoreA", scoreA);
         outState.putInt("ScoreB", scoreB);
+        outState.putInt("FoulA", aFoul);
+        outState.putInt("FoulB", bFoul);
         super.onSaveInstanceState(outState);
     }
 
@@ -60,8 +77,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         scoreA = savedInstanceState.getInt("ScoreA");
         scoreB = savedInstanceState.getInt("ScoreB");
-        teamAScore.setText(String.valueOf(scoreA));
-        teamBScore.setText(String.valueOf(scoreB));
+        aFoul = savedInstanceState.getInt("FoulA");
+        bFoul = savedInstanceState.getInt("FoulB");
+
+        setTeamAScore(scoreA);
+        setTeamBScore(scoreB);
+
+        setTeamAFoul(aFoul);
+        setTeamBFoul(bFoul);
     }
 
     @Override
@@ -69,36 +92,72 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         switch (view.getId()) {
             case R.id.btnTeamAOne:
-                teamAScore.setText(String.valueOf(scoreA += 1));
+                setTeamAScore(scoreA += 1);
                 break;
 
             case R.id.btnTeamATwo:
-                teamAScore.setText(String.valueOf(scoreA += 2));
+                setTeamAScore(scoreA += 2);
                 break;
 
             case R.id.btnTeamAThree:
-                teamAScore.setText(String.valueOf(scoreA += 3));
+                setTeamAScore(scoreA += 3);
                 break;
 
+            case R.id.btnTeamASix:
+                setTeamAScore(scoreA += 6);
+                break;
+
+            case R.id.btnTeamAFoul:
+                setTeamAFoul(++aFoul);
+                break;
 
             case R.id.btnTeamBOne:
-                teamBScore.setText(String.valueOf(scoreB += 1));
+                setTeamBScore(scoreB += 1);
                 break;
 
             case R.id.btnTeamBTwo:
-                teamBScore.setText(String.valueOf(scoreB += 2));
+                setTeamBScore(scoreB += 2);
                 break;
 
             case R.id.btnTeamBThree:
-                teamBScore.setText(String.valueOf(scoreB += 3));
+                setTeamBScore(scoreB += 3);
+                break;
+
+            case R.id.btnTeamBSix:
+                setTeamBScore(scoreB += 6);
+                break;
+
+            case R.id.btnTeamBFoul:
+                setTeamBFoul(++bFoul);
                 break;
 
             case R.id.btnReset:
                 scoreA = 0;
                 scoreB = 0;
-                teamAScore.setText(String.valueOf(scoreA));
-                teamBScore.setText(String.valueOf(scoreB));
+                aFoul = 0;
+                bFoul = 0;
+                setTeamAScore(scoreA);
+                setTeamBScore(scoreB);
+                setTeamAFoul(aFoul);
+                setTeamBFoul(bFoul);
                 break;
         }
+    }
+
+
+    private void setTeamAScore(int score) {
+        teamAScore.setText(String.valueOf(score));
+    }
+
+    private void setTeamAFoul(int foul) {
+        teamAFoul.setText(String.valueOf(foul));
+    }
+
+    private void setTeamBScore(int score) {
+        teamBScore.setText(String.valueOf(score));
+    }
+
+    private void setTeamBFoul(int foul) {
+        teamBFoul.setText(String.valueOf(foul));
     }
 }
